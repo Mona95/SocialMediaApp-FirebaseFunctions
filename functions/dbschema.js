c8917338e10940a72ac8e64cb0a51ceaa73d546e
mona95 <shamona9594@gmail.com>
2020-07-04 16:21:50 +0300

    intializing the firebase cloud functions, signin/signup/ .. other related functions , using express with functions

---
+++ functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: "user",
+      body: "Scream body",
+      createdAt: "2020-07-02T19:30:21.129Z",
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ],
+};
